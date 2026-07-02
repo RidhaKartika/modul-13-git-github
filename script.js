f2e87895de480f03ec1f8ffec52540766abc833b
Tika <2402004@students.poltas.ac.id>
2026-07-02 11:58:52 +0700

    menambahkan perubahan pada fitur deskripsi

---
+++ script.js
@@ -0,0 +1,8 @@
+const deskripsi = document.getElementById("deskripsi");
+const btnUbah = document.getElementById("btnUbah");
+
+btnUbah.addEventListener("click", function () {
+    deskripsi.textContent =
+        "Update Deskripsi berhasil diubah menggunakan JavaScript.";
+    
+});
